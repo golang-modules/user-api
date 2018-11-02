c35f8569a7ea3bc75a00a5a4cdd6b896a04bde32
Chaganti <rchaganti@TX717-0ASHTDH.local>
2018-11-01 21:07:10 -0500

    Added more API's

--- controllers/user.go
+++ controllers/user.go
@@ -1,12 +1,13 @@
 package controllers
 
 import (
+	"log"
 	"strconv"
 	"user-api/models"
 
-	"github.com/golang-modules/users/mysql"
-
 	"github.com/gin-gonic/gin"
+	gmodels "github.com/golang-modules/users/models"
+	gmysql "github.com/golang-modules/users/mysql"
 )
 
 //UserController defines user type
@@ -21,7 +22,7 @@ func NewUserController() *UserController {
 //GetAll to retrieve user
 func (uc UserController) GetAll(c *gin.Context) {
 
-	ul := mysql.New()
+	ul := gmysql.New()
 	u, err := ul.GetAll()
 
 	res := models.Response{}
@@ -43,8 +44,138 @@ func (uc UserController) Get(c *gin.Context) {
 
 	i, _ := strconv.ParseInt(id, 10, 64)
 
-	ul := mysql.New()
+	ul := gmysql.New()
 	u, err := ul.Get(i)
+
+	res := models.Response{}
+	if err != nil {
+		res.StatusCode = 1
+		res.Message = err.Error()
+	} else {
+		res.StatusCode = 1000
+		res.Message = "Success"
+		res.Data = u
+	}
+
+	c.JSON(200, res)
+}
+
+//Create to retrieve user
+func (uc UserController) Create(c *gin.Context) {
+	u := gmodels.User{}
+	err := c.Bind(&u)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	log.Println(u)
+
+	ul := gmysql.New()
+	r, err := ul.Create(u)
+
+	res := models.Response{}
+	if err != nil {
+		res.StatusCode = 1
+		res.Message = err.Error()
+	} else {
+		res.StatusCode = 1000
+		res.Message = "Success"
+		res.Data = r
+	}
+
+	c.JSON(200, res)
+}
+
+//Update to retrieve user
+func (uc UserController) Update(c *gin.Context) {
+	u := gmodels.User{}
+	err := c.Bind(&u)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	log.Println(u)
+
+	ul := gmysql.New()
+	r, err := ul.Update(u)
+
+	res := models.Response{}
+	if err != nil {
+		res.StatusCode = 1
+		res.Message = err.Error()
+	} else {
+		res.StatusCode = 1000
+		res.Message = "Success"
+		res.Data = r
+	}
+
+	c.JSON(200, res)
+}
+
+//UpdateStatus to retrieve user
+func (uc UserController) UpdateStatus(c *gin.Context) {
+	u := gmodels.User{}
+	err := c.Bind(&u)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	log.Println(u)
+
+	ul := gmysql.New()
+	r, err := ul.UpdateStatus(u.ID, u.Active)
+
+	res := models.Response{}
+	if err != nil {
+		res.StatusCode = 1
+		res.Message = err.Error()
+	} else {
+		res.StatusCode = 1000
+		res.Message = "Success"
+		res.Data = r
+	}
+
+	c.JSON(200, res)
+}
+
+//UpdatePassword to retrieve user
+func (uc UserController) UpdatePassword(c *gin.Context) {
+	u := gmodels.User{}
+	err := c.Bind(&u)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	log.Println(u)
+
+	ul := gmysql.New()
+	r, err := ul.UpdatePassword(u.ID, u.Password)
+
+	res := models.Response{}
+	if err != nil {
+		res.StatusCode = 1
+		res.Message = err.Error()
+	} else {
+		res.StatusCode = 1000
+		res.Message = "Success"
+		res.Data = r
+	}
+
+	c.JSON(200, res)
+}
+
+//Delete to retrieve user
+func (uc UserController) Delete(c *gin.Context) {
+	id := c.Param("id")
+
+	i, _ := strconv.ParseInt(id, 10, 64)
+
+	ul := gmysql.New()
+	u, err := ul.Delete(i)
 
 	res := models.Response{}
 	if err != nil {
--- server.go
+++ server.go
@@ -14,6 +14,11 @@ func main() {
 		uc := controllers.NewUserController()
 		user.GET("/", uc.GetAll)
 		user.GET("/:id", uc.Get)
+		user.POST("/", uc.Create)
+		user.PUT("/", uc.Update)
+		user.PATCH("/status", uc.UpdateStatus)
+		user.PATCH("/password", uc.UpdatePassword)
+		user.DELETE("/:id", uc.Delete)
 	}
 
 	router.Run(":9000")
